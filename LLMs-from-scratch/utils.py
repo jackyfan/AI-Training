0abfdc8fe9ba50777ab2d87a06ac2f45bdaf71be
jacky <jackyfan@163.com>
2026-01-14 18:10:14 +0800

    增加工具

---
+++ LLMs-from-scratch/utils.py
@@ -0,0 +1,24 @@
+import re
+
+def create_vocab(text):
+    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+    preprocessed = [item.strip() for item in preprocessed if item.strip()]
+    all_words = sorted(set(preprocessed))
+    vocab = {token: integer for integer, token in enumerate(all_words)}
+    return vocab
+
+
+
+def main():
+    with open("datas/the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    vocab = create_vocab(raw_text)
+    print(len(vocab))
+    for i, item in enumerate(vocab.items()):
+        print(item)
+        if i >= 50:
+            break
+
+
+if __name__ == '__main__':
+    main()
