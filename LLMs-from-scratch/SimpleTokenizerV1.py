7c958439e0224c8d181663811da0f5a2f171190b
jacky <jackyfan@163.com>
2026-01-14 18:10:53 +0800

    简单分词类

--- LLMs-from-scratch/SimpleTokenizerV1.py
+++ LLMs-from-scratch/SimpleTokenizerV1.py
@@ -1,5 +1,22 @@
+import re
+import utils
 
- class SimpleTokenizerV1:
-     def __init__(self,vocab):
-         self.str_to_id = vocab
-         self.id_to_str = {v: k for k, v in vocab.items()}
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_id = vocab
+        self.id_to_str = {v: k for k, v in vocab.items()}
+
+    def encode(self, text):
+        """处理输入文本 ，根据词汇表转化为词元ID"""
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_id[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        """将词元ID转化为文本"""
+        text = " ".join([self.id_to_str[id] for id in ids])
+        text = re.sub(r'([,.?_!"()\']|--)', r'\1', text)
+        return text
+
