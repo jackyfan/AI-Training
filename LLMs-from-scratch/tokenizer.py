431f8f6dc4b2b67d5df3f1bd11e42d986cb9d121
jacky <jackyfan@163.com>
2026-01-15 11:56:46 +0800

    整理tokenizer

--- LLMs-from-scratch/tokenizer.py
+++ LLMs-from-scratch/tokenizer.py
@@ -2,6 +2,25 @@ import re
 import utils
 
 
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_id = vocab
+        self.id_to_str = {v: k for k, v in vocab.items()}
+
+    def encode(self, text):
+        """处理输入文本 ，根据词汇表转化为词元ID"""
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_id[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        """将词元ID转化为文本"""
+        text = " ".join([self.id_to_str[id] for id in ids])
+        text = re.sub(r'([,.?_!"()\']|--)', r'\1', text)
+        return text
+
+
 class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab
@@ -23,7 +42,7 @@ class SimpleTokenizerV2:
         return text
 
 
-def main():
+def test_SimpleTokenizerV2():
     vocab = utils.create_vocab_with_verdict()
     tokenizer = SimpleTokenizerV2(vocab)
     text = """"It's the last he painted, you know," 
@@ -33,9 +52,17 @@ def main():
     print(tokenizer.decode(ids))
     unknown_text = "Hello, do you like tea?"
     # 未知的单词，会报错
-    print( tokenizer.encode(unknown_text))
+    print(tokenizer.encode(unknown_text))
 
 
-
-if __name__ == "__main__":
-    main()
+def test_SimpleTokenizerV1():
+    vocab = utils.create_vocab_with_verdict()
+    tokenizer = SimpleTokenizerV1(vocab)
+    text = """"It's the last he painted, you know," 
+     Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(text)
+    print(ids)
+    print(tokenizer.decode(ids))
+    unknown_text = "Hello, do you like tea?"
+    # 未知的单词，会报错
+    print(tokenizer.encode(unknown_text))
