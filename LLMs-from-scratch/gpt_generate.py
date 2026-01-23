9ef9f41ed1f8b287f03644118a700ccff930cd4e
jacky <jackyfan@163.com>
2026-01-23 14:26:17 +0800

    temperature,topk

--- LLMs-from-scratch/gpt_generate.py
+++ LLMs-from-scratch/gpt_generate.py
@@ -2,6 +2,7 @@ from gpt import GPTModel
 import torch
 import tiktoken
 from gpt import generate_text_simple
+import matplotlib.pyplot as plt
 
 
 def text_to_token_ids(text, tokenizer):
@@ -98,6 +99,104 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
     print(decoded_text)
     model.train()
 
+def test_generate_text_simple_with_temperature():
+    vocab = {
+        "closer": 0,
+        "every": 1,
+        "effort": 2,
+        "forward": 3,
+        "inches": 4,
+        "moves": 5,
+        "pizza": 6,
+        "toward": 7,
+        "you": 8,
+    }
+    inverse_vocab = {v: k for k, v in vocab.items()}
+    next_token_logits = torch.tensor(
+        [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+    )
+    probas = torch.softmax(next_token_logits, dim=0)
+    next_token_id = torch.argmax(probas).item()
+    print(inverse_vocab[next_token_id])
+    torch.manual_seed(123)
+    next_token_id = torch.multinomial(probas, num_samples=1).item()
+    print(inverse_vocab[next_token_id])
+    def print_sampled_tokens(probas):
+         torch.manual_seed(123)
+         sample = [torch.multinomial(probas, num_samples=1).item()
+         for i in range(1_000)]
+         sampled_ids = torch.bincount(torch.tensor(sample))
+         for i, freq in enumerate(sampled_ids):
+            print(f"{freq} x {inverse_vocab[i]}")
+
+    print_sampled_tokens(probas)
+
+    def softmax_with_temperature(logits, temperature):
+        scaled_logits = logits / temperature
+        return torch.softmax(scaled_logits, dim=0)
+
+    temperatures = [1, 0.1, 5]
+    scaled_probas = [softmax_with_temperature(next_token_logits, T)
+                     for T in temperatures]
+    x = torch.arange(len(vocab))
+    bar_width = 0.15
+    fig, ax = plt.subplots(figsize=(5, 3))
+    for i, T in enumerate(temperatures):
+        rects = ax.bar(x + i * bar_width, scaled_probas[i],
+                       bar_width, label=f'Temperature = {T}')
+    ax.set_ylabel('Probability')
+    ax.set_xticks(x)
+    ax.set_xticklabels(vocab.keys(), rotation=90)
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
+
+def test_generate_text_simple_with_topk():
+    vocab = {
+        "closer": 0,
+        "every": 1,
+        "effort": 2,
+        "forward": 3,
+        "inches": 4,
+        "moves": 5,
+        "pizza": 6,
+        "toward": 7,
+        "you": 8,
+    }
+    inverse_vocab = {v: k for k, v in vocab.items()}
+    next_token_logits = torch.tensor(
+        [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+    )
+    probas = torch.softmax(next_token_logits, dim=0)
+    next_token_id = torch.argmax(probas).item()
+    print(inverse_vocab[next_token_id])
+    torch.manual_seed(123)
+    next_token_id = torch.multinomial(probas, num_samples=1).item()
+    print(inverse_vocab[next_token_id])
+    def print_sampled_tokens(probas):
+         torch.manual_seed(123)
+         sample = [torch.multinomial(probas, num_samples=1).item()
+         for i in range(1_000)]
+         sampled_ids = torch.bincount(torch.tensor(sample))
+         for i, freq in enumerate(sampled_ids):
+            print(f"{freq} x {inverse_vocab[i]}")
+
+    print_sampled_tokens(probas)
+
+    top_k = 3
+    top_logits, top_pos = torch.topk(next_token_logits, top_k)
+    print("Top logits:", top_logits)
+    print("Top positions:", top_pos)
+
+    new_logits = torch.where(
+        condition=next_token_logits<top_logits[-1],
+        input=torch.tensor(float('-inf')),
+        other=next_token_logits
+    )
+    print("New logits:", new_logits)
+    topk_probas = torch.softmax(new_logits, dim=0)
+    print(topk_probas)
+
 
 
 if __name__ == "__main__":
@@ -112,13 +211,16 @@ if __name__ == "__main__":
     }
     torch.manual_seed(123)
     model = GPTModel(GPT_CONFIG_124M)
+    model.to("cpu")
     model.eval()
     start_context = "Every effort moves you"
     tokenizer = tiktoken.get_encoding("gpt2")
     token_ids = generate_text_simple(
         model=model,
         idx=text_to_token_ids(start_context, tokenizer),
-        max_new_tokens=10,
+        max_new_tokens=25,
         context_size=GPT_CONFIG_124M["context_length"]
     )
     print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+    test_generate_text_simple_with_topk()
